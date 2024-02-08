bee92d92041aa876cac4ab4da587721619b25afc
1Mashilo <christophermashilo@gmail.com>
2024-02-08 09:36:50 +0200

    Updated import paths for model classes

--- console.py
+++ console.py
@@ -5,16 +5,22 @@
 import cmd
 import re
 from models.base_model import BaseModel
-from models import storage, user, state, city, amenity, place, review
+from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class_home = {
     "BaseModel": BaseModel,
-    "User": user,
-    "State": state,
-    "City": city,
-    "Amenity": amenity,
-    "Place": place,
-    "Review": review,
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review,
 }
 
 class HBNBCommand(cmd.Cmd):
