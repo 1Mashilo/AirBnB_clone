e8155ccda2e86757b7041480515ebaab71e94412
1Mashilo <christophermashilo@gmail.com>
2024-02-10 18:47:52 +0200

    improved the documentation and made the file to adhere to PEP8

--- models/user.py
+++ models/user.py
@@ -1,4 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This module defines the User class, representing a user within the application.
+The User class inherits from the BaseModel class.
+
+Attributes:
+    email (str): The user's email address.
+    password (str): The user's password.
+    first_name (str): The user's first name.
+    last_name (str): The user's last name.
+"""
+
 from models.base_model import BaseModel
+
 
 class User(BaseModel):
     """User class that inherits from BaseModel"""
