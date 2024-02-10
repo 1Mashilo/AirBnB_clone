0d96a7a94336166c3520c3dfcfc97c784ffdad44
1Mashilo <christophermashilo@gmail.com>
2024-02-10 18:37:46 +0200

    Added Review class with documentation

--- models/review.py
+++ models/review.py
@@ -1,4 +1,19 @@
+#!/usr/bin/python3
+
+
+"""
+This module defines the Review class, representing a user-submitted
+review associated with a rental property.
+The Review class inherits from the BaseModel class
+
+Attributes:
+    place_id (str): ID of the Place the review is for.
+    user_id (str): ID of the user who wrote the review.
+    text (str): The content of the review.
+"""
+
 from models.base_model import BaseModel
+
 
 class Review(BaseModel):
     """Review class that inherits from BaseModel"""
