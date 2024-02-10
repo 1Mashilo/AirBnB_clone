2af438a155c052408d10c2cf8b19177f957903c9
1Mashilo <christophermashilo@gmail.com>
2024-02-10 18:21:52 +0200

    improved documentation

--- models/place.py
+++ models/place.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This module defines the Place class, which represents a rental property or accommodation. 
+The Place class inherits from the BaseModel class (likely providing core data-management functionality).
+
+Attributes:
+    city_id (str): ID of the city the place is located in.
+    user_id (str): ID of the owner of the place.
+    name (str): Name of the place.
+    description (str): Description of the place. 
+    number_rooms (int): Number of rooms in the place.
+    number_bathrooms (int): Number of bathrooms.
+    max_guest (int): Maximum number of guests allowed.
+    price_by_night (int): Price per night.
+    latitude (float): Latitude coordinate.
+    longitude (float): Longitude coordinate.
+    amenity_ids (list): List of amenity IDs associated with the place.
+"""
+
 from models.base_model import BaseModel
 
 class Place(BaseModel):
